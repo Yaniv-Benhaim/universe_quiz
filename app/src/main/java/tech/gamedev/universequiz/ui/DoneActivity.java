31cadbcd5bf966487e49a27dad99037d31e6e6fa
godfella777 <ybenhaim777@gmail.com>
2020-05-15 16:03:48 -0700

    Added Facebook Ad

--- app/src/main/java/tech/gamedev/universequiz/ui/DoneActivity.java
+++ app/src/main/java/tech/gamedev/universequiz/ui/DoneActivity.java
@@ -8,12 +8,17 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import com.facebook.ads.*;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -35,10 +40,62 @@ public class DoneActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference question_score;
 
+    private final String TAG = DoneActivity.class.getSimpleName();
+    private InterstitialAd interstitialAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
+
+        AudienceNetworkAds.initialize(this);
+
+        interstitialAd = new InterstitialAd(this, "291898471826789_291899311826705");
+        interstitialAd.setAdListener(new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                interstitialAd.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        });
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd();
+
+
 
         AppRate.with(this)
                 .setInstallDays(0)
@@ -177,6 +234,9 @@ public class DoneActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
+        if (interstitialAd != null) {
+            interstitialAd.destroy();
+        }
         super.onDestroy();
 
         //UNBIND music service
@@ -192,4 +252,28 @@ public class DoneActivity extends AppCompatActivity {
         mp.start();
 
     }
+
+    private void showAdWithDelay() {
+        /**
+         * Here is an example for displaying the ad with delay;
+         * Please do not copy the Handler into your project
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Check if interstitialAd has been loaded successfully
+                if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+                    return;
+                }
+                // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+                if(interstitialAd.isAdInvalidated()) {
+                    return;
+                }
+                // Show the ad
+                interstitialAd.show();
+            }
+        }, 1000); // Show the ad after 15 minutes
+    }
+
+
 }
